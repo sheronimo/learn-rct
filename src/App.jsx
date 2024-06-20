19c8f47e724c024202df9cc2af38c30c0fd7ab45
Shuber <shuberinu@gmail.com>
2024-06-20 17:47:04 +0700

    RCT-1 #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,29 @@
-import { useEffect, useState } from 'react'
-import { NewTodoForm } from './NewTodoForm'
-import { TodoList } from './TodoList'
+import { NavLink, Route, Routes } from 'react-router-dom'
+import { TodoPage } from './pages/Todo'
+import { Home } from './pages/Home'
 import './styles.css'
 
 export default function App() {
-	const [todos, setTodos] = useState(() => {
-		const localValue = localStorage.getItem('ITEMS')
-
-		if (localValue == null) return []
-
-		return JSON.parse(localValue)
-	})
-
-	useEffect(() => {
-		localStorage.setItem('ITEMS', JSON.stringify(todos))
-	}, [todos])
-
-	function addTodo(title) {
-		setTodos((currentTodos) => {
-			return [
-				...currentTodos,
-				{ id: crypto.randomUUID(), title, completed: false }
-			]
-		})
-	}
-
-	function toggleTodo(id, completed) {
-		setTodos((currentTodos) => {
-			return currentTodos.map((todo) => {
-				if (todo.id === id) {
-					return { ...todo, completed }
-				}
-
-				return todo
-			})
-		})
-	}
-
-	function deleteTodo(id) {
-		setTodos((currentTodos) => {
-			return currentTodos.filter((todo) => todo.id !== id)
-		})
-	}
-
 	return (
 		<>
-			<NewTodoForm addTodo={addTodo} />
-			<h1 className='header'>Todo List</h1>
-			<TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+			<header>
+				<nav class='navbar'>
+					<ul>
+						<li>
+							<NavLink to='/'>Home</NavLink>
+						</li>
+						<li>
+							<NavLink to='/todo'>Todo</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/todo' element={<TodoPage />} />
+				</Routes>
+			</main>
 		</>
 	)
 }
